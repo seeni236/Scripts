fc734026a52fe94ccb0a93d6cfb5c471ee1555f7
seenimohamed.a <seenimohamed.a@zohocorp.com>
2017-10-28 23:16:29 +0530

    Modified the comment

--- shell-scripts/find_class.sh
+++ shell-scripts/find_class.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Usage : search particular class in all jars
+# Usage : List all the jars which has the particual class file
 
 #  Command to run : 
-#  ./find_class.sh <source_folder> <class name to search>
+#  ./find_class.sh <source_folder> <class name>
 
 find "$1" -name "*.jar" -exec sh -c 'jar -tf {}|grep -H --label {} '$2'' \;
