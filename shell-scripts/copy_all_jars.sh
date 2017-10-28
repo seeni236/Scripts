2186db5625c6f586b5a8a56e48341ececbe9a459
seenimohamed.a <seenimohamed.a@zohocorp.com>
2017-10-28 23:13:27 +0530

    Added script to move files without extension

--- shell-scripts/copy_all_jars.sh
+++ shell-scripts/copy_all_jars.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-#copy all jar files to one location
+# Usage : copy all jar files to one location
+
+#  Command to run : 
+#  ./cp_all_jars.sh <source_folder> <destination_folder>
 
 find "$1" -name "*.jar" -type file -exec cp {} "$2" \;
 
-""" Command to run : 
- ./cp_all_jars.sh <source_folder> <destination_folder> """
